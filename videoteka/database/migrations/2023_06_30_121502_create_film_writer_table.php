48cbaeefaab5b6622abdb11c58141bce3ccc767f
UrosZikic <uroszikic1512988@gmail.com>
2023-06-30 14:33:23 +0200

    commit videoteka

---
+++ videoteka/database/migrations/2023_06_30_121502_create_film_writer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('film_writer', function (Blueprint $table) {
+            $table->unsignedBigInteger('film_id')->nullable(true);
+            $table->unsignedBigInteger('person_id')->nullable(true);
+
+            $table->primary(['film_id', 'person_id'], 'film_person_pk');
+
+            $table->foreign('film_id', 'writer_film_fk')->references('id')->on('films')
+                ->onUpdate('cascade')->onDelete('no action');
+
+            $table->foreign('person_id', 'writer_person_fk')->references('id')->on('people')
+                ->onUpdate('cascade')->onDelete('no action');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('film_writer');
+    }
+};
